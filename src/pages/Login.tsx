5c6f242c20c07e2ebdf9c790b4545b02d0c4569a
Edinson2005 <ferneyedinson14@gmail.com>
2025-01-27 18:33:10 -0500

    nuevos cambios

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,23 @@
 
 import React from "react";
 import styles from "../styles/Login.module.css"
+import imagen from "../assets/perfil.png"
+
 const Login : React.FC = () => {
     return(
         <>
-        <h1 className={styles.titulo}>hola desde login</h1>
+        <div className={styles.container}>
+            <img src={imagen} className={styles.imagenPerfil}/>
+            <h1>Login</h1>
+
+            <input type="email" placeholder="Ingrese Email" required/> <br/>            
+            <input type="password" placeholder="Contraseña" required/>
+
+            <h1 className={styles.contraseña}>¿Olvidates tu contraseña?</h1>
+
+            <button className={styles.login}>Login</button>
+
+        </div>
         </>
     )
 }
