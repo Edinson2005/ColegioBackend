1e3bd20c55657b8cbf2b51f2081e0c4344e8bcdd
Kevin-nn <thekevincaldon@gmail.com>
2025-01-28 20:36:43 -0500

    conectar backend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 const Home : React.FC = () => {
     return(
         <>
-        <h1 className={styles.titulo}>Hola desde home</h1>
+        <h1 className={styles.titulo}>plataforma school</h1>
         </>
     )
 }
