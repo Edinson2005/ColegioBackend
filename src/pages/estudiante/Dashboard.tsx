1b368d65aa9cada96e3687232b64412ea87bb9b0
Kevin-nn <thekevincaldon@gmail.com>
2025-02-08 18:25:34 -0500

    rutas

--- src/pages/estudiante/Dashboard.tsx
+++ src/pages/estudiante/Dashboard.tsx
@@ -17,11 +17,15 @@ const StudentDashboard: React.FC = () => {
   return (
     <div className="dashboard-container">
       <aside className="sidebar">
+      <Link to="registro" className="sidebar-link">registro</Link>
+
         <Link to="ver-notas" className="sidebar-link">Ver notas</Link>
         <Link to="ver-menu" className="sidebar-link">Ver menú</Link>
         <Link to="horario-clases" className="sidebar-link">Horario de clases</Link>
-        <Link to="docentes" className="sidebar-link">registro</Link>
-        <Link to="/login" className="sidebar-link logout">Salir</Link>
+        <Link to="Docentes" className="sidebar-link">docentes </Link>
+
+
+        <Link to="/" className="sidebar-link logout">Salir</Link>
       </aside>
 
       <main className="main-content">
